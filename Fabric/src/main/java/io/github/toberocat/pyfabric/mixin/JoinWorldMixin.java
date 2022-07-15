a2d7595a7d03ae060c95ad76ceabffe7bbb888d3
Toberocat <tobias.madlberger@gmail.com>
2022-07-15 17:06:26 +0200

    Package

--- Fabric/src/main/java/io/github/toberocat/pyfabric/mixin/JoinWorldMixin.java
+++ Fabric/src/main/java/io/github/toberocat/pyfabric/mixin/JoinWorldMixin.java
@@ -19,8 +19,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 //@Environment(EnvType.CLIENT)
 @Mixin(ClientPlayNetworkHandler.class)
 public class JoinWorldMixin {
-    @Shadow
-    private ClientWorld world;
 
     @Inject(at = @At("TAIL"), method = "onGameJoin(Lnet/minecraft/network/packet/s2c/play/GameJoinS2CPacket;)V")
     private void onGameJoin(GameJoinS2CPacket packet, CallbackInfo ci) {
--- Fabric/src/main/java/io/github/toberocat/pyfabric/mixin/LeaveWorldMixin.java
+++ Fabric/src/main/java/io/github/toberocat/pyfabric/mixin/LeaveWorldMixin.java
@@ -1,4 +1,25 @@
 package io.github.toberocat.pyfabric.mixin;
 
+import io.github.toberocat.pyfabric.PyFabric;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.network.packet.s2c.play.DisconnectS2CPacket;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ClientPlayNetworkHandler.class)
 public class LeaveWorldMixin {
+
+    @Inject(at = @At("TAIL"), method = "onDisconnect(Lnet/minecraft/network/packet/s2c/play/DisconnectS2CPacket;)V")
+    private void onDisconnect(DisconnectS2CPacket packet, CallbackInfo ci) {
+        PyFabric.server.disconnectFromServer();
+    }
+
+    @Inject(at = @At("TAIL"), method = "onDisconnected(Lnet/minecraft/text/Text;)V")
+    private void onDisconnected(Text reason, CallbackInfo ci) {
+        PyFabric.server.disconnectFromServer();
+
+    }
 }
